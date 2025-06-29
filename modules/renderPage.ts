0c0a3e6fd1bf393375d01a040e4b91817d3b703f
sincerajn <sincerajn@outlook.com>
2025-06-29 23:40:30 +0800

    Update template.

--- modules/renderPage.ts
+++ modules/renderPage.ts
@@ -1,24 +1,36 @@
 export function renderPage(data: Map<string, unknown[]>) {
   return `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="utf-8" />
-      </head>
-      <body style="max-width: 600px; margin-inline: auto; line-height: 1.5;">
-        <h1 style="font-family: Monaco, monospace;">zapapi</h1>
-        <p>✧(๑• v•๑) ✧</p>
-        <ul style="list-style: none; padding: 0;">
-          ${Array.from(data.keys())
-            .map(
-              resource =>
-                `<li><a style="color: #003388; font-weight: bold;" href="/${resource}">/${resource}</a> - ${
-                  data.get(resource)?.length ?? 0
-                } items</li>`
-            )
-            .join('')}
-        </ul>
-      </body>
-    </html>
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <style>
+      body {
+        line-height: 1.6;
+        max-width: 600px;
+        margin-inline: auto;
+        @media (prefers-color-scheme: dark) {
+          color: #fff;
+          background-color: #121212;
+        }
+      }
+    </style>
+  </head>
+  <body>
+    <h1 style="font-family: Monaco, monospace">zapapi</h1>
+    <p>✧(๑• v•๑) ✧</p>
+    <ul style="list-style: none; padding: 0">
+      ${Array.from(data.keys())
+        .map(
+          key =>
+            `<li><a style="color: #009688" href="${key}">${key}</a> - ${
+              data.get(key)!.length
+            } items</li>`
+        )
+        .join('')}
+    </ul>
+  </body>
+  </html>
   `
 }
