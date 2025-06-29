7c553f393fc43760a27ef0d4796bf622b34b3420
sincerajn <sincerajn@outlook.com>
2025-06-29 22:49:51 +0800

    提取为独立模块

--- modules/index.ts
+++ modules/index.ts
@@ -1,16 +1,4 @@
 export * from './loadDatabase'
+export * from './response'
 export * from './renderPage'
 export * from './matchByQuery'
-
-export function responseJSON(
-  data: NonNullable<unknown>,
-  status: number = 200
-) {
-  return Response.json(data, {
-    status: status,
-  })
-}
-
-export function responseNotFound(message: string = 'Not Found.') {
-  return responseJSON({ message, code: 404 }, 404)
-}
---
+++ modules/response.ts
@@ -0,0 +1,12 @@
+export function responseJSON(
+  data: NonNullable<unknown>,
+  status: number = 200
+) {
+  return Response.json(data, {
+    status: status,
+  })
+}
+
+export function responseNotFound(message: string = 'Not Found.') {
+  return responseJSON({ message, code: 404 }, 404)
+}
